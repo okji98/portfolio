a9806012030a6a2a228bc51355ba6e38eed2e1b1
okji98 <dnflsmstltk10@gamil.com>
2025-06-27 09:44:55 +0900

    portfolio1 update

--- portfolio1/js/common.js
+++ portfolio1/js/common.js
@@ -1,82 +1,92 @@
-bw = $("body").width();
-$(window).resize(function(){
-    bw = $("body").width();
-});
+// jQuery 로드 확인 후 실행
+$(document).ready(function() {
+    
+    let bw = $("body").width();
+    
+    // 윈도우 리사이즈
+    $(window).resize(function(){
+        bw = $("body").width();
+    });
 
-
-
-//헤더부분
-$(document).ready(function () {
+    // 헤더부분 - 햄버거 메뉴
     $(".hamburger").click(function () {
         $(this).toggleClass("is-active");
     });
-});
-//헤더에 마우스호버됐을 때
-let ln = $(this).index() + 1;
-$(".gnb li").on("mouseover", function () {
-    ln = $(this).index() + 1;
-    $(".snb" + ln).addClass("on").siblings(".snb").removeClass("on");
-    $("header").addClass("on");
-});
-//헤더에서 마우스리브됐을 때
-$("header").on("mouseleave", function () {
-    $(".snb").removeClass("on");
-    $("header").removeClass("on");
-});
-//스크롤시 헤더 색상 변경
-$(window).scroll(function () {
-    let scrT = $(window).scrollTop();
-    if (scrT > .105) {
-        $("header").addClass("fix");
-    } else {
-        $("header").removeClass("fix");
-    }
+
+    // 헤더에 마우스호버됐을 때
+    let ln = 0;
+    $(".gnb li").on("mouseover", function () {
+        ln = $(this).index() + 1;
+        $(".snb" + ln).addClass("on").siblings(".snb").removeClass("on");
+        $("header").addClass("on");
+    });
+
+    // 헤더에서 마우스리브됐을 때
+    $("header").on("mouseleave", function () {
+        $(".snb").removeClass("on");
+        $("header").removeClass("on");
+    });
+
+    // 스크롤시 헤더 색상 변경
+    $(window).scroll(function () {
+        let scrT = $(window).scrollTop();
+        if (scrT > 105) {
+            $("header").addClass("fix");
+        } else {
+            $("header").removeClass("fix");
+        }
+    });
+
+    // 푸터부분
+    $("footer .label").click(function(){
+        $(this).next("footer .m_snb").slideToggle(500);
+    });
+
+    // side 퀵메뉴
+    $("#quick .open").click(function(){
+        $(this).next().addClass("big").removeClass("small");
+    });
+    $("#quick .close").click(function(){
+        $(this).parent().addClass("small").removeClass("big");
+    });
+
+    // 탑버튼
+    $(".top_btn").click(function(){
+        $("html, body").animate({"scrollTop":0}, 500);
+    });
+    
+    // 스크롤 시 탑버튼 표시
+    $(window).scroll(function(){
+        let sc = $(this).scrollTop();
+        if(sc > 0){
+            $(".top_btn").addClass("view");
+        }else{
+            $(".top_btn").removeClass("view");
+        }
+    });
+
+    // 탭메뉴 PC
+    $("header .utile .col").click(function(){
+        $("#tab_menu").addClass("open");
+    });
+    $("#tab_menu .close").click(function(){
+        $("#tab_menu").removeClass("open");
+    });
+
+    // 모바일 탭메뉴
+    $("header .m_utile .col").click(function(){
+        $("#tab_menu_m").addClass("open");
+    });
+    $("#tab_menu_m .close").click(function(){
+        $("#tab_menu_m").removeClass("open");
+    });
+    
+    // 모바일 탭메뉴 슬라이드토글
+    $("#tab_menu_m > ul > li > a").click(function(){
+        $(this).siblings("ul").slideToggle(1000);
+    });
+    
 });
 
-
-//푸터부분
-$("footer .label").click(function(){
-    $(this).next("footer .m_snb").slideToggle(500);
-});
-
-
-//side 퀵메뉴
-$("#quick .open").click(function(){
-    $(this).next().addClass("big").removeClass("small");
-});
-$("#quick .close").click(function(){
-    $(this).parent().addClass("small").removeClass("big");
-});
-
-//탑버튼
-$(".top_btn").click(function(){
-    $("html").animate({"scrollTop":0},500);
-});
-$(window).scroll(function(){
-    let sc = $(this).scrollTop();
-    if(sc > 0){
-        $(".top_btn").addClass("view");
-    }else{
-        $(".top_btn").removeClass("view");
-    }
-});
-
-//탭메뉴
-$("header .utile .col").click(function(){
-    $(".pct").addClass("open");
-});
-$(".pct .close").click(function(){
-    $(".pct").removeClass("open");
-});
-
-//모바일 탭메뉴
-$("header .m_utile .col").click(function(){
-    $("#tab_menu_m").addClass("open");
-});
-$("#tab_menu_m .close").click(function(){
-    $("#tab_menu_m").removeClass("open");
-});
-//모바일 탭메뉴 슬라이드토글
-$("#tab_menu_m > ul > li > a").click(function(){
-    $(this).siblings("ul").slideToggle(1000);
-});
+// 전역 변수로 bw 설정
+let bw = $(window).width();
--- portfolio1/js/sub1_1.js
+++ portfolio1/js/sub1_1.js
@@ -1,5 +1,44 @@
-$(".board_page > a").click(function(){
-    $(this).addClass("select").siblings().removeClass("select");
-    let bn = $(this).index() - 1;
-    $("table").eq(bn).addClass("view").siblings().removeClass("view");
+$(document).ready(function() {
+    
+    // 페이지네이션 클릭 이벤트
+    $(".board_page > a").click(function(e){
+        e.preventDefault(); // 기본 링크 동작 방지
+        
+        // 모든 페이지 버튼에서 select 클래스 제거
+        $(".board_page > a").removeClass("select");
+        // 클릭된 버튼에 select 클래스 추가
+        $(this).addClass("select");
+        
+        // 클릭된 버튼의 인덱스 가져오기 (첫 번째와 마지막 화살표 버튼 제외)
+        let allButtons = $(".board_page > a");
+        let clickedIndex = allButtons.index(this);
+        
+        // 화살표 버튼들을 제외한 실제 페이지 번호 계산
+        let pageButtons = $(".board_page > a").not(':first').not(':last').not(':nth-child(2)').not(':nth-last-child(2)');
+        let pageIndex = pageButtons.index(this);
+        
+        // 유효한 페이지 번호인 경우에만 테이블 변경
+        if (pageIndex >= 0) {
+            // 모든 테이블 숨기기
+            $(".board_list table").removeClass("view");
+            // 해당 인덱스의 테이블 보이기
+            $(".board_list table").eq(pageIndex).addClass("view");
+        }
+    });
+    
+    // 검색 기능
+    $(".board_title form").submit(function(e) {
+        e.preventDefault();
+        // 여기에 검색 로직 추가
+        console.log("검색 실행");
+    });
+    
+    // 검색 버튼 클릭
+    $(".board_title button").click(function(e) {
+        e.preventDefault();
+        // 검색 실행
+        let searchTerm = $("#search_input").val();
+        console.log("검색어:", searchTerm);
+    });
+    
 });
